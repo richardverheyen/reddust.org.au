20c102ab7579c6682bbd94d6248fd694048a4779
Jan Werkhoven <jan.werkhoven@gmail.com>
2016-11-03 23:56:14 +1100

    Initial Grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,256 @@
+module.exports = function(grunt) {
+
+  'use strict';
+
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: 'dist',
+          hostname: 'localhost',
+          livereload: true,
+          open: false
+        }
+      }
+    },
+
+    watch: {
+      handlebars: {
+        files: ['src/templates/**/*.hbs', 'src/templates/**/*.json', 'src/templates/layout.html '],
+        tasks: ['build-HTML']
+      },
+      sass: {
+        files: ['src/styles/**/*.scss'],
+        tasks: ['build-CSS']
+      },
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['build-JS']
+      },
+      assets: {
+        files: ['src/public/**/*'],
+        tasks: ['copy']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js'],
+        tasks: ['build']
+      },
+      options: {
+        livereload: true,
+      }
+    },
+
+    handlebarslayouts: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src/templates/',
+          src: ['**/*.hbs', '!partials/*'],
+          dest: 'dist/',
+          ext: '.html',
+        }],
+        options: {
+          partials: ['src/templates/partials/*.hbs', 'src/templates/layout.html'],
+          basePath: 'src/templates/',
+          modules: ['src/templates/helpers/helpers-*.js'],
+          context: {
+            // formLink: '/can-I-transfer-my-UK-pension-to-Australian-super',
+            formLink: '/form',
+            formTitle: 'Can I transfer my UK Pension to Australian Super?',
+            pathPensionTransferGuide: '/assets/PDF/Pension-Transfer-Guide-by-FinSec-PTX.pdf'
+          }
+        }
+      }
+    },
+
+    sass: {
+      options: {
+        sourcemap: 'none',
+        noCache: true
+      },
+      raw: {
+        options: {
+          style: 'expanded',
+        },
+        files: {
+          'dist/assets/css/main.css': ['src/styles/main.scss']
+        }
+      },
+      minified: {
+        options: {
+          style: 'compressed',
+        },
+        files: {
+          'dist/assets/css/main.min.css': ['src/styles/main.scss']
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require('autoprefixer')({
+            browsers: ['> 1% in AU', 'IE > 9']
+          })
+        ]
+      },
+      raw: {
+        src: 'dist/assets/css/main.css'
+      },
+      minified: {
+        src: 'dist/assets/css/main.min.css'
+      }
+    },
+
+    jshint: {
+      files: ['src/js/main.js'],
+      options: {
+        force: true,
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'temp/js/main.min.js': ['src/js/main.js'],
+          'temp/js/localstorage_safari_private_shim.min.js': ['src/js/localstorage_safari_private_shim.js'],
+          'dist/js/outdated-browser.min.js': ['src/js/outdated-browser.js']
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';\n\n',
+      },
+      raw: {
+        files: {
+          'dist/assets/js/main.js': [
+            'bower_components/jquery/dist/jquery.min.js',
+            'src/js/localstorage_safari_private_shim.js',
+            'src/js/main.js'
+          ]
+        }
+      },
+      minified: {
+        files: {
+          'dist/assets/js/main.min.js': [
+            'bower_components/jquery/dist/jquery.min.js',
+            'temp/js/localstorage_safari_private_shim.min.js',
+            'temp/js/main.min.js'
+          ]
+        }
+      }
+    },
+
+    clean: {
+      dist: {
+        src: ['dist/']
+      },
+      temp: {
+        src: ['temp/']
+      }
+    },
+
+    copy: {
+      main: {
+        files: [{
+          expand: true,
+          cwd: 'src/public/',
+          src: ['**'],
+          dest: 'dist/'
+        }]
+      }
+    },
+
+    // BUG: Adds /dist to every URL...
+    xml_sitemap: {
+      custom_options: {
+        options: {
+          siteRoot: 'http://www.finsecptx.com.au/',
+          changefreq: 'monthly',
+          priority: '0.5',
+          dest: 'dist/'
+        },
+        files: [{
+          expand: true,
+          cwd: 'dist/',
+          src: ['**/*.html', '!**/google*.html', '!**/emails/*.html'],
+        }]
+      }
+    },
+
+    // Temporary until sitemap bug is fixed
+    replace: {
+      sitemap_dist: {
+        src: 'dist/sitemap.xml',
+        dest: 'dist/sitemap.xml',
+        replacements: [{
+          from: '/dist/',
+          to: '/'
+        }, {
+          from: '<feed>',
+          to: ''
+        }, {
+          from: '</feed>',
+          to: ''
+        }]
+      }
+    },
+
+    // inlinecss: {
+    //   main: {
+    //     options: {
+    //       removeStyleTags: false
+    //     },
+    //     files: [{
+    //       src: 'dist/emails/letter-of-authority.html',
+    //       dest: 'dist/emails/letter-of-authority-inline.html'
+    //     }, {
+    //       src: 'dist/emails/confirmation-email.html',
+    //       dest: 'dist/emails/confirmation-email-inline.html'
+    //     }]
+    //   }
+    // }
+
+  });
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-html-prettyprinter');
+  // grunt.loadNpmTasks('grunt-inline-css');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-xml-sitemap');
+  grunt.loadNpmTasks("grunt-handlebars-layouts");
+
+  // Available commands
+  grunt.registerTask('default', ['build', 'sitemap', 'serve']);
+  grunt.registerTask('build', ['clean', 'copy', 'build-HTML', 'build-CSS', 'build-JS']);
+  grunt.registerTask('build-HTML', ['handlebarslayouts']);
+  grunt.registerTask('build-CSS', ['sass', 'postcss']);
+  grunt.registerTask('build-JS', ['jshint', 'uglify', 'concat', 'clean:temp']);
+  grunt.registerTask('sitemap', ['xml_sitemap', 'replace:sitemap_dist']);
+  grunt.registerTask('serve', ['connect', 'watch']);
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# reddust.org.au
+# Red Dust website
+
+#### What
+The [website](http://www.reddust.org.au/) of a health development organisation in remote indigenous communities
+
+#### Highlights
+* Built with Grunt, Handlebars, SASS, Atom & Git
+* Mobile responsive & retina-ready
+* Served on SSD Ubuntu Nginx servers
+* Design by [Hannah Sutton](http://hannahsuttondesign.com/)
+* Code by [Jan Werkhoven](https://github.com/janwerkhoven) and [Richard Verheyen](https://github.com/richardverheyen)
+
+#### Setup
+1. Make sure you have [Grunt](http://gruntjs.com/getting-started), [NPM](https://nodejs.org/en/download/) and [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) installed globally
+2. `git clone https://github.com/janwerkhoven/grunt-finsec-ptx.git ~/janwerkhoven/grunt-finsec-ptx`
+3. `cd ~/janwerkhoven/grunt-finsec-ptx`
+4. `npm install; bower install`
+5. `grunt`
+5. Go to [http://localhost:9000/](http://localhost:9000/)
+6. Open code with whatever rocks your boxers, I use [Atom](https://atom.io/): `atom .`
+7. Happy coding!
+
+#### Contact
+Get in touch on [LinkedIn](https://au.linkedin.com/pub/jan-werkhoven/10/64/b30), [GitHub](https://github.com/janwerkhoven) or <a href="mailto:jw@nabu.io" target="_blank">jw@nabu.io</a>.
+
+--------------
+
+**Jan Werkhoven**  
+Web Developer  
+& UI Designer
