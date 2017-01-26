bbf707e0fc2cc42053cf820eb9c2314a194b9550
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-01-27 01:31:09 +1100

    Remove non-project files

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,17 @@ var del = require('del');
 var nunjucksRender = require('gulp-nunjucks-render');
 var prettify = require('gulp-jsbeautifier');
 var data = require('gulp-data');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Watch all the files and run specific tasks if one changes
-gulp.task('watch', function() {
-  gulp.watch(['src/public/**/*'], ['copy-public']);
-  gulp.watch(['src/templates/**/*.+(html|nunjucks)', 'src/data/**/*.json'], ['compile-html']);
-  gulp.watch(['src/styles/**/*.scss'], ['scss']);
-  gulp.watch(['src/js/**/*.js'], ['js']);
-});
+// gulp.task('watch', function() {
+//   gulp.watch(['src/public/**/*'], ['copy-public']);
+//   gulp.watch(['src/templates/**/*.+(html|nunjucks)', 'src/data/**/*.json'], ['compile-html']);
+//   gulp.watch(['src/styles/**/*.scss'], ['scss']);
+//   gulp.watch(['src/js/**/*.js'], ['js']);
+// });
 
 // Delete the dist folder
 gulp.task('delete-dist', function() {
@@ -40,4 +43,24 @@ gulp.task('compile-html', ['delete-dist'], function() {
     .pipe(nunjucksRender({ path: ['src/templates'] }))
     .pipe(prettify({ config: './prettify.json' }))
     .pipe(gulp.dest('dist'))
+});
+
+// Compile all CSS to dist folder
+gulp.task('compile-css', ['delete-dist'], function() {
+  return gulp.src('src/styles/imports.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['> 1% in AU', 'Explorer > 9', 'Firefox >= 17', 'Chrome >= 10', 'Safari >= 6', 'iOS >= 6'],
+      cascade: false
+    }))
+    .pipe(rename('styles.css'))
+    .pipe(gulp.dest('dist/assets/css'))
+});
+
+// Minify the CSS file
+gulp.task('minify-css', ['compile-css'], function() {
+  return gulp.src('dist/assets/css/styles.css')
+    .pipe(cleanCSS())
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('dist/assets/css'));
 });
